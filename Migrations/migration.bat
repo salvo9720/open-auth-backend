9ef9420e7249bc3d3e2a419a462e3453549a55fb
salvatore celano <salvatore.celano@ennova-research.com>
2026-09-10 18:01:57 +0200

    feat migration files bat and fix in appsettigns.json

---
+++ Migrations/migration.bat
@@ -0,0 +1,17 @@
+﻿@echo off
+
+
+for %%F in (*.*) do if /I not "%%~xF"==".bat" del /q "%%F"
+
+cd ..
+
+
+echo Pulizia completata.
+
+dotnet ef migrations add InitialCreate
+echo creazione migrazione da EF dalle classi 
+
+dotnet ef database update
+echo Database aggiornato correttamente.
+
+pause
