0908200f1289c0c5d67a801effd35f5b0ab77b64
salvatore celano <salvatore.celano@ennova-research.com>
2026-09-16 16:45:07 +0200

    feat: test with xunit using new project form vs interface

---
+++ Tests/open-auth-backend-Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace open_auth_backend_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Console.WriteLine("Test for fn");
+            Assert.True(true);
+        }
+    }
+}
---
+++ open-auth-backend.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace open_auth_backend.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
